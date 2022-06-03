fc132bc62132153b88df194a83a3417582bc9c8b
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-06-03 10:36:20 -0600

    models

---
+++ keepr/Models/VaultKeep.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace keepr.Models
+{
+    public class VaultKeep
+    {
+        public int Id { get; set; }
+        public string CreatorId { get; set; }
+        public int VaultId { get; set; }
+        public int KeepId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
