f4d8adb4db83f9a193940e05d5bbb27c6d54aada
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-06-03 11:48:43 -0600

    keeps controller

---
+++ keepr/Controllers/KeepsController.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using keepr.Services;
+using keepr.Models;
+using Microsoft.AspNetCore.Authorization;
+using CodeWorks.Auth0Provider;
+
+namespace keepr.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KeepsController : ControllerBase
+    {
+        private readonly KeepsService _serv;
+
+        public KeepsController(KeepsService serv)
+        {
+            _serv = serv;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Keep>> GetAll()
+        {
+            try
+            {
+                List<Keep> found = _serv.GetAll();
+                return Ok(found);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Keep> GetById(int id)
+        {
+            try
+            {
+                Keep found = _serv.GetById(id);
+                return Ok(found);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<Keep>> Create([FromBody] Keep data)
+        {
+            try
+            {
+                Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+                data.CreatorId = userInfo.Id;
+                Keep created = _serv.Create(data);
+                return Ok(created);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [Authorize]
+        public async Task<ActionResult<Keep>> Edit(int id, [FromBody] Keep update)
+        {
+            try
+            {
+                Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+                update.Id = id;
+                update.CreatorId = userInfo.Id;
+                Keep edited = _serv.Edit(update);
+                return Ok(edited);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<ActionResult<Keep>> Remove(int id)
+        {
+            try
+            {
+                Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+                Keep removed = _serv.Remove(id, userInfo.Id);
+                return Ok(removed);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
