90ed7aeb3fc8c114b441620d58dd0677c348ddd6
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-06-13 14:49:07 -0600

    added placeholder to vaults if you have none

---
+++ keepr/wwwroot/assets/AboutPage.0cd283b0.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.a89a9ca9.js";import{o,b as t,d as s}from"./vendor.d2861f43.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(d,i,p,u,f,h){return o(),t("div",c,r)}var b=e(a,[["render",_]]);export{b as default};
